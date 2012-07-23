1038bcd4948227b8a107f425ed1217b391669287
Mateu X Hunter <hunter@missoula.org>
2012-07-23 12:12:37 -0600

    version bump, changes note

--- lib/MooX/Types/MooseLike.pm
+++ lib/MooX/Types/MooseLike.pm
@@ -5,7 +5,7 @@ use Exporter 5.57 'import';
 use Module::Runtime qw(require_module);
 use Carp qw(confess);
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 sub register_types {
   my ($type_definitions, $into, $moose_namespace) = @_;
