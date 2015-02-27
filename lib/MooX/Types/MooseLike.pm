97c0cbcbaab53a255df47c52efb34516aff84533
Mateu X Hunter <hunter@missoula.org>
2015-02-27 12:10:24 -0700

    Bump version

--- lib/MooX/Types/MooseLike.pm
+++ lib/MooX/Types/MooseLike.pm
@@ -8,7 +8,7 @@ use Module::Runtime qw(require_module);
 use Carp qw(confess croak);
 use List::Util qw(first);
 
-our $VERSION = '0.27';
+our $VERSION = '0.28';
 
 sub register_types {
   my ($type_definitions, $into, $moose_namespace) = @_;
